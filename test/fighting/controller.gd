47641bcf640e7ca18586bf0953c928697781780e
streq <strequena@gmail.com>
2021-11-11 07:27:37 -0300

    controllers

--- test/fighting/controller.gd
+++ test/fighting/controller.gd
@@ -1,16 +1,25 @@
 extends Node
 
+export (int) var pj
+
+onready var U = "up"+str(pj)
+onready var D = "down"+str(pj)
+onready var L = "left"+str(pj)
+onready var R = "right"+str(pj)
+onready var A = "A"+str(pj)
+onready var B = "B"+str(pj)
+
 func get_direction():
 	var input_direction = Vector2()
-	input_direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	input_direction.x = int(Input.is_action_pressed(R)) - int(Input.is_action_pressed(L))
+	input_direction.y = int(Input.is_action_pressed(D)) - int(Input.is_action_pressed(U))
 	return input_direction
 
 func get_jump():
-	return Input.is_action_just_pressed("A")
+	return Input.is_action_just_pressed(A)
 	
 func is_pressing_jump():
-	return Input.is_action_pressed("A")
+	return Input.is_action_pressed(A)
 
 func get_attack():
-	return Input.is_action_just_pressed("B")
+	return Input.is_action_just_pressed(B)
