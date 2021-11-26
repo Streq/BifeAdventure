18d7054c52bc08c4afbc54ac7a0ed71e7d7cc6bc
streq <strequena@gmail.com>
2021-11-26 09:07:14 -0300

    overworld preload of every map, building hometown

--- global/globals/globals.gd
+++ global/globals/globals.gd
@@ -1,9 +1,18 @@
 extends Node
 
+enum ROOM {
+	my_room,
+	my_living_room,
+	my_hometown,
+	my_hometown_basement,
+	endurance_round
+}
+
+
 var step_counter : int = 0 setget set_step_counter
 
 var spawn_tile = null
 
 func set_step_counter(val):
 	step_counter = val
-	print_debug("steps:", val)
+#	print_debug("steps:", val)
--- overworld/tilemap/trigger/scene_change_trigger.gd
+++ overworld/tilemap/trigger/scene_change_trigger.gd
@@ -1,8 +1,10 @@
 extends "trigger.gd"
 
-export(String, FILE, "*.tscn") var to
+
+export(Globals.ROOM) var to = Globals.ROOM.my_hometown
+
 
 func trigger(pawn, grid, cell_start, cell_target):
 	yield(pawn, "finish_step")
 	Globals.spawn_tile = grid.world_to_map($spawn_pos.global_position)
-	get_tree().change_scene(to)
+	get_tree().change_scene_to(Overworld.map[to])
