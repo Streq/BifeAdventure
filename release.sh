3544ede59136c853cb7751e5d1a1d24edc4ca5a0
streq <strequena@gmail.com>
2021-12-18 16:26:27 -0300

    now we got the release I think it's final

--- release.sh
+++ release.sh
@@ -5,7 +5,9 @@ godot --export "HTML5"
 git add .
 git stash
 git checkout gh-pages || exit "$?"
-git checkout stash -- .
+git rm -rf .
+git checkout HEAD -- .gitignore
+git stash pop
 git commit -m"release"
 git push
 git checkout main
