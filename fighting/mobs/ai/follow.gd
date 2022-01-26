0e37532aa334022ac9d5222be36a7fae7144fd21
streq <strequena@gmail.com>
2022-01-26 09:32:28 -0300

    turn on wall ai

--- fighting/mobs/ai/follow.gd
+++ fighting/mobs/ai/follow.gd
@@ -1,13 +1,6 @@
-extends "res://util/state/state.gd"
+extends State
 
-var timeout := false
-
-	
 func handle(character, controller):
 	if controller.target:
 		var dist : Vector2 = controller._target_distance()
 		controller.direction.x = sign(dist.x)
-
-
-func _on_Timer_timeout():
-	timeout = true
---
+++ fighting/mobs/ai/walk_and_flip_on_wall.gd
@@ -0,0 +1,18 @@
+extends State
+
+export var frame_reaction_time := 2
+export var dir := 1.0
+
+var frames = 0
+
+func enter():
+	frames = 0
+
+func handle(character : KinematicBody2D, controller):
+	if character.is_on_wall():
+		if !frames:
+			dir = -character.dir
+		frames = (frames + 1)%frame_reaction_time
+	else:
+		frames = 0
+	controller.direction.x = dir
