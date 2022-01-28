7d0c174b1c309e58b37a001389cb23521a305315
streq <strequena@gmail.com>
2022-01-28 09:15:17 -0300

    fixed the kill message on the forest

--- scenes/fighting/bosque/bosque.gd
+++ scenes/fighting/bosque/bosque.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
-
+var lost = false
 func player_lose():
-	$Timer.start()
-	yield($Timer,"timeout")
-	$textbox.add_text("uh, ehhh")
-	$textbox.add_text("voy a hacer como que eso no paso")
-	yield($textbox,"text_display_finished")
-	Globals.respawn()
+	if !lost:
+		lost = true
+		$Timer.start()
+		yield($Timer,"timeout")
+		$textbox.add_text("uh, ehhh")
+		$textbox.add_text("voy a hacer como que eso no paso")
+		yield($textbox,"text_display_finished")
+		Globals.respawn()
+
+func _on_textbox_text_display_started(textbox):
+	get_tree().paused = true
+
+
+func _on_textbox_text_display_finished(textbox):
+	get_tree().paused = false
