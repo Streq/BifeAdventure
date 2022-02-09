b023b6d57a45705801a706a7f5f59bd108fc8b00
streq <strequena@gmail.com>
2022-02-08 21:05:20 -0300

    a little prouder now

--- fighting/mobs/thrower/patrol.gd
+++ fighting/mobs/thrower/patrol.gd
@@ -1,26 +1,22 @@
 extends State
 
 export var frame_reaction_time := 2
+export var strategy : NodePath
 export var dir := 1.0
 
-onready var peace = $walk_around
-onready var attack = $aim_attack
+onready var peace = "walk_around"
+onready var attack = "aim_attack"
 onready var sight = $sight
 onready var current = peace
 
 
-func handle(character : KinematicBody2D, controller):
-	sight.scale.x = character.dir
-	return current.handle(character, controller)
+func _physics_process(delta):
+	sight.scale.x = owner.dir
 
 func _on_sight_body_entered(body):
 	if body.is_in_group("player"):
-		current.exit()
-		current = attack
-		current.enter()
+		get_node(strategy)._change_state(attack, null)
 
 func _on_sight_body_exited(body):
 	if body.is_in_group("player"):
-		current.exit()
-		current = peace
-		current.enter()
+		get_node(strategy)._change_state(peace, null)
