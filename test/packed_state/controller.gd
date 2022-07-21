6feba75e2b2f9d8f4ecff39de480f219d067d222
streq <strequena@gmail.com>
2022-07-21 07:24:31 -0300

    fix controller jump at the end of dialog

--- test/packed_state/controller.gd
+++ test/packed_state/controller.gd
@@ -6,13 +6,22 @@ export var enabled : bool = true setget set_enabled
 func set_enabled(val):
 	enabled = val
 	set_physics_process(enabled)
+	if enabled:
+		#to prevent jumping at the end of a dialog
+		update_inputs()
+		get_parent().input_state.clear_updated()
 	if !enabled:
-		 get_parent().input_state.clear()
+		get_parent().input_state.clear()
+	
 func _physics_process(delta):
+	update_inputs()
+func update_inputs():
 	var input = get_parent().input_state
+
 	input.A.update(Input.is_action_pressed("A0"))
 	input.B.update(Input.is_action_pressed("B0"))
 	input.C.update(Input.is_action_pressed("C0"))
+
 	input.dir.x = float(Input.is_action_pressed("right0")) - float(Input.is_action_pressed("left0"))
 	if flip_h:
 		input.dir.x *= -1.0
