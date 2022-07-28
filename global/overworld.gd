58b60715e4e4849a94769d704cd6031365fc460b
streq <strequena@gmail.com>
2022-07-28 09:51:49 -0300

    background done, ahora si, a arrancar con los enemigos

--- global/overworld.gd
+++ global/overworld.gd
@@ -12,7 +12,7 @@ const map = {
 	Globals.ROOM.malovich_room : preload("res://scenes/overworld/malovich_room.tscn"),
 	Globals.ROOM.malovich_room_right_after_fight : preload("res://scenes/overworld/malovich_room_right_after_fight/scene.tscn"),
 	Globals.ROOM.malovich_fight_room : preload("res://scenes/fighting/malovich/malovich_fight.tscn"),
-	Globals.ROOM.hometown_forest : preload("res://scenes/fighting/bosque/bosque.tscn"),
+	Globals.ROOM.hometown_forest : preload("res://scenes/fighting/bosque/bosque_nuevo.tscn"),
 	Globals.ROOM.boquita : preload("res://scenes/overworld/boquita.tscn")
 }
 
