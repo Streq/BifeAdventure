04affadacc1c8492eea851fba2ee4c9861335f5b
streq <strequena@gmail.com>
2022-07-29 09:59:12 -0300

    rebound del cosito cuando el ataque choca contra la pared

--- fighting/distributed_fighter/fighters/cosito/state/attack.gd
+++ fighting/distributed_fighter/fighters/cosito/state/attack.gd
@@ -12,3 +12,6 @@ func _physics_update(delta):
 		root.velocity.x = lerp(root.velocity.x, 0.0, root.idle_lerp*delta)
 	elif !root.is_on_floor():
 		$on_floor.enabled = true
+	if root.is_on_wall():
+		root.velocity.x = -root.get_facing_dir()*50.0
+		goto_args("rebound",[10])
